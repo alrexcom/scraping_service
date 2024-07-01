2ce496c0316127d1998e511ae241bd4883d98196
Alrexcom <mpstmprt@mail.ru>
2024-07-01 12:06:05 +0300

    Page

---
+++ src/scraping_service/views/view.py
@@ -0,0 +1,10 @@
+import datetime
+
+from django.shortcuts import render
+
+
+def get_home_page(request):
+    date = datetime.datetime.now().date()
+    name = 'Alex'
+    _context = {'name': name, 'date': date}
+    return render(request, 'home.html', _context)
